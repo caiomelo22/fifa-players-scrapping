c32a9af7b8ed722f4d983ff7371e6f55a23005cf
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-07-26 10:10:34 -0300

    Saving images to a pre-defined directory set in the env variables

--- src/helpers/image/helper.py
+++ src/helpers/image/helper.py
@@ -6,17 +6,24 @@ from selenium.webdriver.common.by import By
 
 class ImageHelper:
     def __init__(self) -> None:
-        pass
+        self.dir_path = os.environ.get('IMAGE_DIR_PATH')
+        if self.dir_path is None:
+            raise Exception("Environment variable IMAGE_DIR_PATH is not set.")
 
-    def save_image(self, img, directory, file_name):
+    def save_image_to_env_folder(self, img, directory, file_name):
+        # Create the 'dist' folder within the specified path if it doesn't exist
+        dist_folder = os.path.join(self.dir_path, directory)
+        os.makedirs(dist_folder, exist_ok=True)
+
+        # Prepare the image content from the URL
         image_content = requests.get(img).content
         image_file = io.BytesIO(image_content)
-        directory = f"dist/{directory}"
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        file_path = f"{directory}/{file_name}"
 
-        open(file_path, "wb").write(image_file.getbuffer())
+        # Save the image to the specified folder and filename
+        file_path = os.path.join(dist_folder, file_name)
+        with open(file_path, "wb") as f:
+            f.write(image_file.getbuffer())
+        
         return file_path
 
     def get_img_url(self, element):
